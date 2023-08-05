06356635166069244036523184e1d4cd2cbac3a2
Gavin Chait <whythawk@gmail.com>
2023-08-05 23:23:20 +0200

    Update activity.ts

--- frontend/api/activity.ts
+++ frontend/api/activity.ts
@@ -4,7 +4,7 @@ import { apiCore } from "./core"
 export const apiActivity = {
   // ACTIVITIES
   async getMulti(token: string, payload: IActivityFilters = {}) {
-    return await useFetch<IActivity[]>(`${apiCore.url()}/activity`, 
+    return await useFetch<IActivity[]>(`${apiCore.url()}/activity/`, 
       {
         headers: apiCore.headers(token),
         query: payload,
