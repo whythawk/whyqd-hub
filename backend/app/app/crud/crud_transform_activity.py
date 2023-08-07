4cc235dc29ceda10689a68448a5a3ded6d8d3e33
Gavin Chait <whythawk@gmail.com>
2023-08-07 07:21:08 +0200

    Update crud_transform_activity.py

--- backend/app/app/crud/crud_transform_activity.py
+++ backend/app/app/crud/crud_transform_activity.py
@@ -6,6 +6,7 @@ from fastapi.encoders import jsonable_encoder
 from app.crud.base import CRUDBase
 from app.models import User, Subscription, TransformActivity
 from app.schemas import TransformActivityCreate, TransformActivityUpdate
+from app.crud.crud_product import product as crud_product
 from app.core.config import settings
 
 
@@ -19,18 +20,19 @@ class CRUDTransformActivity(CRUDBase[TransformActivity, TransformActivityCreate,
         data_import: bool = True,
         subscription: Subscription | None = None,
     ) -> TransformActivity:
-        if settings.USE_STRIPE and not user.is_superuser:
+        if settings.USE_STRIPE and not user.is_superuser and not subscription.override:
+            product = crud_product.get_by_subscription_type(db=db, subscription_type=subscription.subscription_type)
             # Will blow up if subscription is None
-            if subscription.rows >= row_count:
-                e = f"Subscription row-limit ({subscription.rows}) exceeded (uploaded {row_count} rows)."
+            if row_count > product.rows:
+                e = f"Subscription row-limit ({product.rows}) exceeded (uploaded {row_count} rows)."
                 raise ValueError(e)
             date_to = datetime.utcnow()
             date_from = date_to - timedelta(days=30)
             transform_count = user.transform_activities.filter(
                 (TransformActivity.created >= date_from) & (TransformActivity.created <= date_to)
             ).count()
-            if subscription.transforms >= transform_count:
-                e = f"Subscription transform-limit ({subscription.transforms}) exceeded."
+            if transform_count > product.transforms:
+                e = f"Subscription transform-limit ({product.transforms}) exceeded."
                 raise ValueError(e)
         obj_in = TransformActivityCreate(**{"rows": row_count, "data_import": data_import, "researcher_id": user.id})
         obj_in_data = jsonable_encoder(obj_in)
